ac64b8e2a6f6a4fee00847503199db0e7ddce60a
rulzzbot <pencildrawera@gmail.com>
2025-12-17 14:53:32 -0800

    fixing for host1

--- SCAN AP/afc-tech-app-backend/app.py
+++ SCAN AP/afc-tech-app-backend/app.py
@@ -39,8 +39,4 @@ def create_app():
 app=create_app()
 
 if __name__ == "__main__":
-    app = create_app()
-    #this runs for my home computer lol
     app.run(debug=True, host="0.0.0.0")
-    #app.run(debug=True, host="192.168.1.131")
-
