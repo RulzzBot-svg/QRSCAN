cb1ff139cd4e178f9ba25b0982d8945dc2461e77
rulzzbot <pencildrawera@gmail.com>
2025-12-18 08:50:35 -0800

    fixing PWA9

--- SCAN AP/afc-tech-app/vite.config.js
+++ SCAN AP/afc-tech-app/vite.config.js
@@ -1,8 +1,28 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import mkcert from "vite-plugin-mkcert";
 
+
+
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react()
-  ]
-});
+    tailwindcss(),
+    react(),
+  ],
+  server:{
+    port:5173,
+    host:true,
+    proxy:{
+      "/api":{
+        //home
+        //target:"http://192.168.1.167:5000",
+        //work
+        target:"http://192.168.1.131:5000",
+        changeOrigin:true,
+        secure:false,
+      },
+    },
+  },
+})
