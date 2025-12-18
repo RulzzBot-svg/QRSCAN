92a85b05bd1d394636f25dbf1d3966daa4c15062
RulzzBot-svg <robskalion@mgail.com>
2025-12-17 16:28:40 -0800

    fixing for vercel

--- SCAN AP/afc-tech-app/src/api/api.js
+++ SCAN AP/afc-tech-app/src/api/api.js
@@ -4,8 +4,10 @@ import axios from "axios";
 export const API = axios.create({
 
     //home API
-    baseURL: "/api",
+    //baseURL: "/api",
 
     //work API
     //baseURL:"http://192.168.1.131:5000/api",
+
+    baseURL:import.meta.env.VITE_API_BASE_URL + "/api",
 });
