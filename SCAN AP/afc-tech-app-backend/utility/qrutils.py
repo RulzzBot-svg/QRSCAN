7894d6c503ae0a15d320775f21df18c59a9f711b
rulzzbot <pencildrawera@gmail.com>
2026-02-02 12:49:50 -0800

    its workingggg

--- SCAN AP/afc-tech-app-backend/utility/qrutils.py
+++ SCAN AP/afc-tech-app-backend/utility/qrutils.py
@@ -1,20 +1,29 @@
 import qrcode
+import os
+from app import create_app
+from db import db
+from models import AHU
 
+app = create_app()
 
-ahu_id="H1-ah-013"
+with app.app_context():
+    ahus = AHU.query.all()
+    for ahu in ahus:
+        ahu_id = ahu.id
+        url = f"https://qrscan-lyart.vercel.app/FilterInfo/{ahu_id}"
 
-url= f"https://qrscan-lyart.vercel.app/FilterInfo/{ahu_id}"
+        qr = qrcode.QRCode(
+            version=2,
+            error_correction=qrcode.constants.ERROR_CORRECT_Q,
+            box_size=10,
+            border=4
+        )
 
-qr=qrcode.QRCode(
-    version=2,
-    error_correction=qrcode.constants.ERROR_CORRECT_Q,
-    box_size=10,
-    border=4
-)
+        qr.add_data(url)
+        qr.make(fit=True)
 
-qr.add_data(url)
-qr.make(fit=True)
+        img = qr.make_image(fill_color="black", back_color="white")
+        img.save(f"qr_codes/{ahu_id}.png")
+        print(f"QR generated for {ahu_id}")
 
-img = qr.make_image(fill_color="black", back_color="white")
-img.save(f"{ahu_id}Final.png")
-print("QR generated succesfully!")
+print("All QR codes generated successfully!")
